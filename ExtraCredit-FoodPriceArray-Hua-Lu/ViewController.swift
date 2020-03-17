87c3676b206a89ff492b87f764304b7f5f0e5441
helloconey <48701450+helloconey@users.noreply.github.com>
2020-03-16 19:19:45 -0700

    Initial Commit

---
+++ ExtraCredit-FoodPriceArray-Hua-Lu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExtraCredit-FoodPriceArray-Hua-Lu
+//
+//  Created by Coney Lu on 3/16/20.
+//  Copyright © 2020 Hua Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
